c2a7430947b3083af091ce6bc2178fd8d4f6967b
Rafal Szwedo <rafal.szwedo@gmail.com>
2021-05-08 17:16:57 +0200

    added buttons action

---
+++ script.js
@@ -0,0 +1,86 @@
+const startBtn = document.querySelector('.start');
+const pauseBtn = document.querySelector('.pause');
+const stopBtn = document.querySelector('.stop');
+const resetBtn = document.querySelector('.reset');
+const historyBtn = document.querySelector('.history');
+const stopwatch = document.querySelector('.stopwatch');
+const time = document.querySelector('.time');
+const timeList = document.querySelector('.time-list');
+const infoBtn = document.querySelector('.info');
+const modalShadow = document.querySelector('.modal-shadow');
+const closeModalBtn = document.querySelector('.close');
+
+let countTime;
+let minutes = 0;
+let seconds = 0;
+let timesArr = [];
+
+const handleStart = () => {
+    clearInterval(countTime)
+    countTime = setInterval(() => {
+        
+        if(seconds < 9) {
+            seconds++;
+            stopwatch.textContent = `${minutes}:0${seconds}`;
+        } else if(seconds >= 9 && seconds < 59) {
+            seconds++;
+            stopwatch.textContent = `${minutes}:${seconds}`;
+        } else {
+            minutes++;
+            seconds = 0;
+            stopwatch.textContent = `${minutes}:00`;
+        }
+       
+    }, 100);
+
+}
+
+const handlePause = () => {
+    clearInterval(countTime);
+}
+
+const handleStop = () => {
+
+    time.innerHTML = `Ostatni czas: ${stopwatch.textContent}`;
+
+    if(stopwatch.textContent !== '0:00') {
+        time.style.visibility = 'visible';
+        timesArr.push(stopwatch.textContent);
+    }
+    clearStuff();
+}
+
+const handleReset = () => {
+
+    time.style.visibility = 'hidden';
+    timesArr = [];
+    clearStuff();
+
+}
+
+const clearStuff = () => {
+
+    clearInterval(countTime);
+    minutes = 0;
+    seconds = 0;
+    stopwatch.textContent = `${minutes}:0${seconds}`;
+    timeList.textContent = '';
+
+}
+
+const showHistory = () => {
+
+    timeList.textContent = '';
+    timesArr.forEach((time, index) => {
+        const newTime = document.createElement('li');
+        newTime.innerHTML = `Pomiar nr ${index + 1} <span>${time}</span>`;
+        timeList.appendChild(newTime);
+    })
+
+}
+
+startBtn.addEventListener('click', handleStart);
+pauseBtn.addEventListener('click', handlePause);
+stopBtn.addEventListener('click', handleStop);
+resetBtn.addEventListener('click', handleReset);
+historyBtn.addEventListener('click', showHistory);
